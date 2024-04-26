0d571fee8aaec9df9c20e4341dbd5634d7e51205
shz-code <web.shahidul.alam@gmail.com>
2024-04-26 15:34:17 +0600

    backend: (nearby places api implementation)

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,4 +1,5 @@
 const _ = require("lodash");
+const axios = require("axios");
 
 module.exports.searchLocation = async (req, res) => {
   const { location, radius } = req.body;
@@ -20,5 +21,31 @@ module.exports.myLocation = async (req, res) => {
   return res.send({
     status: true,
     myLocation: _.pick(location, ["address.town"]),
+  });
+};
+
+module.exports.nearbyLocations = async (req, res) => {
+  const { lat, lon, radius } = req.body;
+
+  //   Calculate bounding box
+  const lat_min = lat - radius / 111.1;
+  const lat_max = lat + radius / 111.1;
+  const lon_min = lon - radius / (111.1 * Math.cos((lat * 3.14159) / 180));
+  const lon_max = lon + radius / (111.1 * Math.cos((lat * 3.14159) / 180));
+
+  const place_types = ["hotel"];
+  const query_params = place_types.join("|");
+
+  console.log(query_params);
+
+  const getPlaces = await fetch(
+    `https://nominatim.openstreetmap.org/search?q=${query_params}&format=jsonv2&bounded=1&viewbox=${lon_min},${lat_min},${lon_max},${lat_max}`
+  );
+
+  const places = await getPlaces.json();
+
+  return res.send({
+    status: true,
+    places: places,
   });
 };
--- backend/routes/searchRouter.js
+++ backend/routes/searchRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   searchLocation,
   myLocation,
+  nearbyLocations,
 } = require("../controllers/searchController");
 const router = express.Router();
 
 router.route("/myLocation").post(myLocation);
+router.route("/nearbyPlaces").post(nearbyLocations);
 router.route("/").post(searchLocation);
 
 module.exports = router;
