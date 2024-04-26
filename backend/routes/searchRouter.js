4b5eb0bb0b80d3c7a0df03434dc04d8245ba1916
shz-code <web.shahidul.alam@gmail.com>
2024-04-27 01:31:26 +0600

    backend: {get place details}

--- backend/routes/searchRouter.js
+++ backend/routes/searchRouter.js
@@ -3,12 +3,15 @@ const {
   searchLocation,
   myLocation,
   nearbyLocations,
+  getDetails,
 } = require("../controllers/searchController");
 const router = express.Router();
 
 router.route("/myLocation").post(myLocation);
 router.route("/nearbyPlaces").post(nearbyLocations);
 router.route("/").post(searchLocation);
+
+router.route("/:id").get(getDetails);
 
 module.exports = router;
 // localhost:4003/api/search/myLocation
