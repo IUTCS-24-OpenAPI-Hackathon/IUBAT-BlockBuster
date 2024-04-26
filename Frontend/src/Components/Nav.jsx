97d441a67267938f05da7bc98ed57afe1b1de77e
Miraz PC <abdullahmiraz2015@gmail.com>
2024-04-27 02:14:49 +0600

    Updated

--- Frontend/src/Components/Nav.jsx
+++ Frontend/src/Components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
     { path: "/Contact", link: "Contact Us" }
   ]
   return (
-    <header className='bg-black fixed top-0 right-0 left-0'>
+    <header className='bg-black  top-0 right-0 left-0'>
       <nav className='px-5 py-5 max-w-7xl mx-auto flex justify-between'>
         <a href="" className='text-lg font-bold text-white'>Location<span className='text-orange-400'>Bluster</span></a>
         <ul className='md:flex gap-12 text-lg hidden'>
