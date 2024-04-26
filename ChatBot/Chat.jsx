79b7d74c7cdb83714cdfe41bfbbf5bfd15296498
Apurba <nazmussakibapurbo@gmail.com>
2024-04-27 05:43:49 +0600

    Full details

---
+++ ChatBot/Chat.jsx
@@ -0,0 +1,36 @@
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
+
+import React from 'react';
+
+const Chat = () => {
+    return (
+        <div>
+<div style={{ position: "relative", height: "500px" }}>
+  <MainContainer>
+    <ChatContainer>
+      <MessageList>
+        <Message
+          model={{
+            message: "Hello my friend",
+            sentTime: "just now",
+            sender: "Joe",
+          }}
+        />
+      </MessageList>
+      <MessageInput placeholder="Type message here" />
+    </ChatContainer>
+  </MainContainer>
+</div>; 
+            
+        </div>
+    );
+};
+
+export default Chat;
--- Frontend/src/Pages/Fulldetails.jsx
+++ Frontend/src/Pages/Fulldetails.jsx
@@ -2,13 +2,22 @@
 
 const Fulldetails = ({ features }) => {
   const { formatted } = features.details.features[0].properties;
-  console.log(features.weather);
+  console.log(features);
+  const { main, visibility, wind } = features.weather;
+  const { name, categories, city, country, lat, lon } = features.details.features[0].properties
   // const { details } = features.details.features[0].properties;
   return (
-    <div>
+    <div className="bg-slate-300 rounded-md p-2">
       <h3 className="title"></h3>
       <p className="font-bold">Address: {formatted}</p>
-      <div className="flex justify-center my-5"></div>
+      <p className="font-bold">Temperature: {main.temp}</p>
+      <div className="font-bold">Visibility: {visibility}</div>
+      <p className="font-bold">Wind Speed: {wind.speed} Degree:{wind.deg}</p>
+      <p className='font-bold'>Category: {categories[0], categories[1]}</p>
+      <p className='font-bold'>Name: {name}</p>
+      <p className='font-bold'>City: {city}</p>
+      <p className='font-bold'>Country: {country}</p>
+      <p>lat={lat} and lon= {lon}</p>
     </div>
   );
 };
